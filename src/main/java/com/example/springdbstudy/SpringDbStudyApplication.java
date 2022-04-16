f9d42b2c7da6c131ca67d01cedbb5d6c966bcd24
woo00oo <96woo94@naver.com>
2022-04-16 13:16:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springdbstudy/SpringDbStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdbstudy/SpringDbStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdbstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
