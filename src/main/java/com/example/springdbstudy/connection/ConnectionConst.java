f37df78e8bfa1874ec34feb750b0c13dcd2dace2
woo00oo <96woo94@naver.com>
2022-04-16 20:04:19 +0900

    데이터베이스 연결

---
+++ src/main/java/com/example/springdbstudy/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package com.example.springdbstudy.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/data/springdb";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
---
+++ src/test/java/com/example/springdbstudy/connection/DBConnectionUtilTest.java
@@ -0,0 +1,21 @@
+package com.example.springdbstudy.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+
+}
