3da077240a71cad549abdc191da244124e5f2c6e
woo00oo <96woo94@naver.com>
2022-04-18 22:57:37 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/com/example/springdbstudy/connection/ConnectionTest.java
+++ src/test/java/com/example/springdbstudy/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.example.springdbstudy.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -23,9 +24,23 @@ class ConnectionTest {
     }
 
     @Test
-    void dataSourceDriverManager() throws SQLException {
+    void dataSourceDriverManager() throws SQLException, InterruptedException {
         //DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        userDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
         userDataSource(dataSource);
     }
     
